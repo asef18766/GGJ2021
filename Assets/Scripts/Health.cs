ecd49437ab9a3fe83f05bf078d64c8211081bbc7
skps2010 <skps2010030908@yahoo.com>
2021-01-31 02:21:42 +0800

    add health bar

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MainGame.Player;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private PlayerProp playerProp;
+    [SerializeField] private PlayerProp curPlayerProp;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float ratio = (float)curPlayerProp.maxHealth / playerProp.maxHealth;
+        float width = ((RectTransform)transform).rect.width;
+        transform.localPosition = new Vector3(-width * (0.5f - ratio / 2), 0, 0);
+        transform.localScale = new Vector3(ratio, 1, 1);
+    }
+}
