6ae4392d338fd0c0841c79b5e82161bf0169d6d2
markchen0819 <markchen0819@gmail.com>
2021-01-30 16:30:13 +0800

    切換LostFound功能
    - 新增TileMap
    - 新增TileMap用的Materials
    - 新增測試用美術圖
    - 新增Mode功能

---
+++ Assets/Scripts/MainGame/Player/FollowCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    private Vector2 velocity = Vector2.zero;
+
+    public float smoothTime = 0.1f;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 destination = target.position + offset;
+        Vector2 smoothPosition = Vector2.SmoothDamp(transform.position, destination,ref velocity, smoothTime);
+        transform.position = new Vector3(smoothPosition.x, smoothPosition.y, transform.position.z);
+
+    }
+}
---
+++ Assets/Scripts/ModeAndMap/ModeConditions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ModeConditions
+{
+    public Modes mode;
+    public bool CanCollide;
+    public bool CanShow;
+    public Material material;
+}
