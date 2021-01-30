62417cc3d3b2819118b1da996d85ee376d55e708
skps2010 <skps2010030908@yahoo.com>
2021-01-30 15:06:18 +0800

    add cards

---
+++ Assets/Scripts/Report/Card.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if (transform.position.y < 500) return;
+        Debug.Log(((RectTransform)transform).localPosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Report/CardController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardController : MonoBehaviour
+{
+    private GameObject[] cards = new GameObject[3];
+    public GameObject card;
+    public GameObject canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            cards[i] = Instantiate(card, canvas.transform);
+            ((RectTransform)cards[i].transform).localPosition = new Vector3(-200 + i * 200, 0, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
